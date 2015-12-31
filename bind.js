72e64ef4a53afd76b86eff576792c88df440b418
Lochan Chhetri <lochan.chhetri@gmail.com>
2015-12-30 19:39:55 -0800

    bind
    Partial Application with bind function. Bind function can be used to
    pass fix number of arguments to a function permanently.

--- bind.js
+++ bind.js
@@ -18,3 +18,19 @@ var boundNum = retrieveNum.bind(module);
 
 // outputs: 20
 console.log( boundNum() );
+
+
+// we can use bind to permanently fix number of arguments we pass to the function
+function greetings(salutation, name){
+  console.log('Hello ' + salutation + ' ' + name);
+}
+
+// passing null since function doesn't depend on value of 'this'
+var male = greetings.bind(null, "Mr.");
+var female = greetings.bind(null, "Ms.");
+
+// Outputs: Hello Mr. John
+male("John");
+
+// Outputs: Hello Ms. Jane
+female("Jane");
